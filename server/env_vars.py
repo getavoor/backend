97dfe645d09801c2bc82865b123b269a12248932
githubcatw <17374742+githubcatw@users.noreply.github.com>
2026-05-07 15:38:08 +0400

    switch Google App Engine mail backend to SMTP
    this is to resolve stability issues with the legacy mail api

--- server/env_vars.py
+++ server/env_vars.py
@@ -1,7 +1,7 @@
 """
 Avoor - server
 Environment variable loader
-(c) 2024 githubcatw
+(c) 2024-2026 githubcatw
 """
 
 # Load variables from the .env file, if it exists
@@ -32,6 +32,31 @@ def get_env_or_exit(name: str) -> str:
     # Otherwise, return the variable
     return var
 
+
+# First, check if this is running on App Engine by seeing if a variable defined in app.yaml exists
+if os.environ.get("APPENGINE_USE_SMTP_MAIL_SERVICE", None) is not None:
+    # If it is, resolve Google secrets:
+    print("Resolving Google secrets...")
+
+    from google.cloud import secretmanager
+    client = secretmanager.SecretManagerServiceClient()
+
+    def load_variable(name: str):
+        """
+        Load the value of a variable from Google Secret Manager.
+
+        This function assumes that the variable with the given name has its corresponding
+        secret ID (`projects/(project_id)/secrets/(secret_name)/versions/(version)`)
+        as the value. It will load the secret with this ID and replace the value with it
+        (i.e. `os.environ[name]` will return the loaded secret, not its ID).
+        """
+        secret_name = os.environ.get(name, None)
+        if secret_name is not None:
+            os.environ[name] = client.access_secret_version(secret_name)
+
+    # Load variables defined as Google secrets in app.yaml
+    load_variable("APPENGINE_SMTP_PASSWORD")
+
 # Load required environment variables
 DB_SECRET_KEY = get_env_or_exit("AVR_DB_SK")
 DB_URI = get_env_or_exit("AVR_DB_URL")
@@ -40,17 +65,18 @@ SOCK_SECRET_KEY = get_env_or_exit("AVR_SI_SK")
 TOKEN_SALT = get_env_or_exit("AVR_TK_SL")
 EMAIL_ADDRESS = get_env_or_exit("AVR_EM_AD")
 
-if EMAIL_ADDRESS.endswith("appspotmail.com"):
-    EMAIL_PASSWORD = "GAEIgnore"
-    try:
-        import google.appengine.api.mail
-        print("App Engine email detected. No password is required.")
-    except ImportError:
-        print("To use Google App Engine email, install the App Engine bundled services SDK:")
-        print("pip3 install appengine-python-standard>=1.0.0")
-        print('(Or set the environment variable AVR_EM_AD to an address that doesn\'t end with "appspotmail.com" to disable App Engine email)')
+# detect App Engine SMTP using the same method as above
+if os.environ.get("APPENGINE_USE_SMTP_MAIL_SERVICE", None) is not None:
+    # check if a password was loaded using the default App Engine variable
+    pw = os.environ.get("APPENGINE_SMTP_PASSWORD", None)
+    if pw == None:
+        print("Avoor can't work without the environment variable APPENGINE_SMTP_PASSWORD being set.")
+        print("Please set this variable to the ID of a valid Google Secrets Manager secret and restart the Avoor server.")
         exit()
+    else:
+        EMAIL_PASSWORD = "GAEIgnore"
 else:
+    # if not on App Engine, AVR_EM_PW is mandatory
     EMAIL_PASSWORD = os.environ.get("AVR_EM_PW", None)
     if EMAIL_PASSWORD == None:
         print("Avoor can't work without the environment variable AVR_EM_PW being set.")
